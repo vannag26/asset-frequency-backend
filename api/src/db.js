fdd555fc17e308d45b6a4b444a5c328773d0904b
vannag26 <vanna@vdgmanagement.com>
2026-04-14 11:07:06 -0400

    Add api/src/db.js

---
+++ api/src/db.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  max: 20,
+  idleTimeoutMillis: 30_000,
+  connectionTimeoutMillis: 2_000,
+});
+
+module.exports = pool;
